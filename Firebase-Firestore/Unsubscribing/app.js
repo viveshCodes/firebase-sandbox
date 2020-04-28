382948221304b9d3273902db26afca617f86fa3e
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-28 20:26:11 +0545

    Add concept of unsubscribing changes

---
+++ Firebase-Firestore/Unsubscribing/app.js
@@ -0,0 +1,78 @@
+const list = document.querySelector('ul');
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+
+// add recipe to html template 
+const addRecipe = (recipe , id) =>{
+    let time = recipe.created_at.toDate();
+    let html = `
+    <li data-id="${id}">   
+        <div>${recipe.title}</div>
+        <div>${time}</div>
+        <button class = "btn btn-danger btn-sm my-2">delete</button>
+    </li>
+    `;
+
+  list.innerHTML += html;
+}
+
+// delete recipe from html template
+const deleteRecipe = (id) =>{
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe =>{
+        if(recipe.getAttribute('data-id') === id){
+            recipe.remove();
+        }
+    })
+}
+//getting recipes from database
+const unsub = db.collection('recipes').onSnapshot(snapshot =>{
+    snapshot.docChanges().forEach(change =>{
+        const doc = change.doc;
+        if(change.type === 'added'){
+            addRecipe(doc.data() , doc.id);
+        } else {
+            deleteRecipe(doc.id);
+        }
+    })
+});
+
+// add documents to database
+form.addEventListener('submit' , event =>{
+    event.preventDefault();
+
+    const now = new Date();
+    const recipe = {
+        title :form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    }
+    form.reset();
+    db.collection('recipes').add(recipe)
+        .then(()=>{
+            console.log("Recipe added.");
+        }).catch((err =>{
+            console.log(err);
+        }));
+});
+
+// delete from database
+list.addEventListener('click' , event =>{
+    console.log(event);
+    if(event.target.tagName === 'BUTTON'){
+        const id = event.target.parentElement.getAttribute('data-id');
+       db.collection('recipes').doc(id).delete()    // .doc() method is used to get reference to document we want to delete
+       // we have passed 'id' as argument id doc() to get reference to document with this id
+        .then(()=>{
+            console.log("recipe deleted");
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+});
+
+// unsubscribe from changes
+button.addEventListener('click' , () => {
+    // onSnapshot returns a function , and on invoking that function , changes get unsubscribed
+    unsub();
+    console.log("Changes unsubscribed");
+});
