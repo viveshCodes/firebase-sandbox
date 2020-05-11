81a79662d943325c75dd82a528bb34e7c96e69f4
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-11 16:53:13 +0545

    Add custom claims

---
+++ Firebase-Authentication/Custom Claims/scripts/index.js
@@ -0,0 +1,63 @@
+const guideList = document.querySelector('.guides');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
+
+const setupUI = (user) =>{
+  if(user){
+    db.collection('users').doc(user.uid).get().then(doc =>{
+         // setup Account details
+        const html =`
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+      `;
+       accountDetails.innerHTML = html;
+    })
+ 
+    // toggle UI elements
+    loggedInLinks.forEach(item =>item.style.display = 'block');
+    loggedOutLinks.forEach(item =>item.style.display = 'none' );
+  }else{
+    // hide account details if user is logged out
+    accountDetails.innerHTML = ``;
+    
+    // toggle UI elements
+    loggedInLinks.forEach(item =>item.style.display = 'none');
+    loggedOutLinks.forEach(item =>item.style.display = 'block' );
+  }
+
+}
+
+
+// set up guides
+const setupGuides = (data) =>{
+  if(data.length){
+    let html = ``;
+    data.forEach(doc =>{
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4">${guide.title}</div>
+          <div class="collapsible-body white">${guide.content}</div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  }else{
+    guideList.innerHTML = '<h4 class="center-align">Login to view guides</h4>';
+  }
+  
+}
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
+
+  
